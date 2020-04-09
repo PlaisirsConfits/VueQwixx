7b86d0dadc12c6e1472914c411400405d098912d
mo <morgane.bourguenolle@live.fr>
2020-04-09 18:33:21 +0200

    V1 en cours

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <Dice :value="white1DiceValue" color="white"> </Dice>
+    <Dice :value="white2DiceValue" color="white"> </Dice>
+    <Dice :value="redDiceValue" color="red"> </Dice>
+    <Dice :value="yellowDiceValue" color="yellow" > </Dice>
+    <Dice :value="blueDiceValue" color="blue"> </Dice>
+    <Dice :value="greenDiceValue" color="green" > </Dice>
+    <Button @click="rollTheDice">Lancer les dés! </Button>
+
+    <div > Choix possibles : {{white1DiceValue + white2DiceValue}} </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dice from './components/Dice.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Dice
+  },
+  data: function(){
+    return {
+      
+    redDiceValue:5,
+    yellowDiceValue:5,
+    blueDiceValue: 5,
+    greenDiceValue: 5,
+    white1DiceValue: 5,
+    white2DiceValue: 5,
+
+    whiteDices: Number,
+    }
+  },
+  methods: {
+    rollTheDice: function(){
+     console.log(this.$children[0])
+      for(let i=0; i<this.$children.length; i++ ) {
+        let newValue= this.$children[i]
+        newValue.value= Math.floor(Math.random() * 6) + 1  
+        this.$children.splice(i, 1, newValue)
+      }
+     console.log(this.$children)
+     
+    },
+    
+  }, 
+
+  computed : {
+    // redDiceChoices: function(){
+    //   console.log(this)
+    //  let whiteDices = this.white1DiceValue+this.white2DiceValue
+    //   return whiteDices
+    // }
+  },
+ 
 }
 </script>
 
---
+++ src/components/Dice.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div >
+      <img :src="'/assets/'+value+'.svg.png'" :style="{'background-color':color}" />
+
+    
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dice",
+
+  props: {
+    
+    value: {
+      type: Number,
+      default: 1
+    },
+    color: String,
+  },
+  data: function() {
+    return {
+    
+    };
+  },
+  methods: {},
+
+  computed: {
+    
+    
+  }
+};
+</script>
+
