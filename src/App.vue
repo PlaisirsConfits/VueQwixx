5109d436430e28c5e6cc0206a9aaa3e341318ac5
mo <morgane.bourguenolle@live.fr>
2020-04-13 12:56:57 +0200

    choix des dés

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,96 @@
 <template>
   <div id="app">
-    
-    <Dice :value="white1DiceValue" color="white"> </Dice>
-    <Dice :value="white2DiceValue" color="white"> </Dice>
-    <Dice :value="redDiceValue" color="red"> </Dice>
-    <Dice :value="yellowDiceValue" color="yellow" > </Dice>
-    <Dice :value="blueDiceValue" color="blue"> </Dice>
-    <Dice :value="greenDiceValue" color="green" > </Dice>
-    <Button @click="rollTheDice">Lancer les dés! </Button>
+    <Dice :value="white1DiceValue" color="white"></Dice>
+    <Dice :value="white2DiceValue" color="white"></Dice>
+    <Dice :value="redDiceValue" color="red"></Dice>
+    <Dice :value="yellowDiceValue" color="yellow"></Dice>
+    <Dice :value="blueDiceValue" color="blue"></Dice>
+    <Dice :value="greenDiceValue" color="green"></Dice>
+    <Button @click="rollTheDice">Lancer les dés!</Button>
 
-    <div > Choix possibles : {{white1DiceValue + white2DiceValue}} </div>
+    <div>Choix possibles : {{redDiceChoices}}</div>
+    <div>yo : {{redLineChoices}}</div>
   </div>
 </template>
 
 <script>
-import Dice from './components/Dice.vue'
+import Dice from "./components/Dice.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Dice
   },
-  data: function(){
+  data: function() {
     return {
-      
-    redDiceValue:5,
-    yellowDiceValue:5,
-    blueDiceValue: 5,
-    greenDiceValue: 5,
-    white1DiceValue: 5,
-    white2DiceValue: 5,
+      redDiceValue: 5,
+      yellowDiceValue: 5,
+      blueDiceValue: 5,
+      greenDiceValue: 5,
+      white1DiceValue: 5,
+      white2DiceValue: 5,
 
-    whiteDices: Number,
-    }
+      whiteDices: Number,
+      white1AndRedDices: Number,
+      white2AndRedDices: Number
+    };
   },
   methods: {
-    rollTheDice: function(){
-     console.log(this.$children[0])
-      for(let i=0; i<this.$children.length; i++ ) {
-        let newValue= this.$children[i]
-        newValue.value= Math.floor(Math.random() * 6) + 1  
-        this.$children.splice(i, 1, newValue)
-      }
-     console.log(this.$children)
-     
-    },
-    
-  }, 
-
-  computed : {
-    // redDiceChoices: function(){
-    //   console.log(this)
-    //  let whiteDices = this.white1DiceValue+this.white2DiceValue
-    //   return whiteDices
-    // }
+    rollTheDice: function() {
+      this.redDiceValue = Math.floor(Math.random() * 6) + 1;
+      this.yellowDiceValue = Math.floor(Math.random() * 6) + 1;
+      (this.blueDiceValue = Math.floor(Math.random() * 6) + 1),
+        (this.greenDiceValue = Math.floor(Math.random() * 6) + 1);
+      this.white1DiceValue = Math.floor(Math.random() * 6) + 1;
+      this.white2DiceValue = Math.floor(Math.random() * 6) + 1;
+    }
   },
- 
-}
+  computed: {
+    redDiceChoices: function() {
+      let whiteDices = this.white1DiceValue + this.white2DiceValue;
+      let white1AndRedDices = this.white1DiceValue + this.redDiceValue;
+      let white2AndRedDices = this.white2DiceValue + this.redDiceValue;
+      return { whiteDices, white1AndRedDices, white2AndRedDices };
+    },
+
+    redLineChoices: function() {
+      let choice = "";
+      if (
+        this.redDiceChoices.whiteDices >=
+          this.redDiceChoices.white1AndRedDices &&
+        this.redDiceChoices.whiteDices >=
+         this.redDiceChoices.white2AndRedDices
+      ) {
+        choice =
+          "Une combinaison au choix ";
+      } else if (
+        this.redDiceChoices.whiteDices <
+          this.redDiceChoices.white1AndRedDices &&
+        this.redDiceChoices.whiteDices > this.redDiceChoices.white2AndRedDices
+      ) {
+        choice =
+          "Soit les 'whiteDices' puis le 'white1AndRedDices' soit juste le'white2AndRedDices'  ";
+      } else if (
+        this.redDiceChoices.whiteDices <
+          this.redDiceChoices.white2AndRedDices &&
+        this.redDiceChoices.whiteDices > this.redDiceChoices.white1AndRedDices
+      ) {
+        choice =
+          "Soit les 'whiteDices' puis le 'white2AndRedDices' soit juste le'white1AndRedDices'  ";
+      } else if (
+        this.redDiceChoices.whiteDices <
+          this.redDiceChoices.white1AndRedDices &&
+        this.redDiceChoices.whiteDices < this.redDiceChoices.white2AndRedDices
+      ) {
+        choice =
+          "Le'whiteDices' puis le 'white1AndRedDices' OU 'white2AndRedDices'  ";
+      }
+      console.log(choice);
+
+      return choice;
+    }
+  }
+};
 </script>
 
 <style>
